88c364c2bf69761bc48114667ebef662bd427e2f
mcpe500 <ivan04.android@gmail.com>
2023-09-08 22:14:13 +0700

    API class

---
+++ src/Classes/API.ts
@@ -0,0 +1,25 @@
+export class API {
+    public apiRequest() {
+        const apiUrl = 'https://84b0-118-99-84-2.ngrok-free.app';
+
+        const customHeaders = new Headers();
+        // customHeaders.append('Content-Type', 'application/json');
+        // customHeaders.append('name', 'Reditto4');
+        // customHeaders.append('password', 'redittothebest');
+
+        async function fetchData() {
+            try {
+                const response = await fetch(apiUrl, {
+                    method: 'GET',
+                    headers: customHeaders,
+                });
+                const responseData = await response.json();
+                alert(JSON.stringify(responseData));
+                return JSON.parse(responseData);
+            } catch (error) {
+
+            }
+        }
+        return fetchData();
+    }
+} 
