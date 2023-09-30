344908aada2fca0b554082139cad80273c373a85
mcpe500 <ivan04.android@gmail.com>
2023-09-30 21:05:14 +0700

    change Book.ts to IConsumable

--- src/Classes/Items/Book.ts
+++ src/Classes/Items/Book.ts
@@ -1,9 +1,9 @@
-import { IEquippable } from "./IEquippable";
+import { IConsumable } from "./IConsumable";
 import { Item } from "./Item";
 
 //book is not equipable, rather a consumeable.
 
-export abstract class Book extends Item implements IEquippable {
+export abstract class Book extends Item implements IConsumable {
     constructor(imagePath: string, itemName: string, itemDesc: string, itemPrice: number) {
         super(imagePath, itemName, itemDesc, itemPrice);
     }
