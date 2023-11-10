99fc0168ffd6c5453611f1028e7d02595697249c
Fransiscus Xaverius <xaverius.fransiscus078@gmail.com>
2023-11-10 14:44:22 +0700

    Added inspect disable

--- dist/bundle.js
+++ dist/bundle.js
@@ -5722,6 +5722,27 @@ window.onload = () => __awaiter(void 0, void 0, void 0, function* () {
         // if (key === "3") {
         //   game.getPlayer().setEquipmentLevels(3);
         // }
+        document.addEventListener('contextmenu', function (e) {
+            e.preventDefault();
+        });
+        document.onkeydown = function (e) {
+            // disable F12 key
+            if (e.keyCode == 123) {
+                return false;
+            }
+            // disable I key
+            if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
+                return false;
+            }
+            // disable J key
+            if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+                return false;
+            }
+            // disable U key
+            if (e.ctrlKey && e.keyCode == 85) {
+                return false;
+            }
+        };
         if (key === "i") {
             //destroy top entity
             //for destroying crates, and stone entities.
--- src/main.ts
+++ src/main.ts
@@ -249,6 +249,32 @@ window.onload = async () => {
     //   game.getPlayer().setEquipmentLevels(3);
     // }
 
+    document.addEventListener('contextmenu', function(e) {
+      e.preventDefault();
+    });
+    document.onkeydown = function (e) {
+
+    // disable F12 key
+    if(e.keyCode == 123) {
+        return false;
+    }
+
+    // disable I key
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 73){
+        return false;
+    }
+
+    // disable J key
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+        return false;
+    }
+
+    // disable U key
+    if(e.ctrlKey && e.keyCode == 85) {
+        return false;
+    }
+    }
+
     if (key === "i") {
       //destroy top entity
       //for destroying crates, and stone entities.
